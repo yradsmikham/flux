0be0f561cdb924cc39b3658a8ea6f9feac86239e
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-04-22 15:54:45 -0700

    added new counter metrics

--- cluster/kubernetes/metrics.go
+++ cluster/kubernetes/metrics.go
@@ -1,15 +1,17 @@
 package kubernetes
 
-import (
-	"github.com/go-kit/kit/metrics/prometheus"
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
-)
+// var (
+// 	fluxSuccessCounter = prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+// 		Namespace: "flux",
+// 		Subsystem: "kubernetes",
+// 		Name:      "flux_kubcetl_apply_successes",
+// 		Help:      "Counter that depicts the number of successes",
+// 	}, []string{})
 
-var (
-	fluxFailures = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
-		Namespace: "flux",
-		Subsystem: "kubernetes",
-		Name:      "flux_kubcetl_apply_failures",
-		Help:      "Counter that depicts the number of failures that have occurred",
-	}, []string{})
-)
+// 	fluxFailCounter = prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+// 		Namespace: "flux",
+// 		Subsystem: "kubernetes",
+// 		Name:      "flux_kubcetl_apply_failures",
+// 		Help:      "Counter that depicts the number of failures",
+// 	}, []string{})
+// )
